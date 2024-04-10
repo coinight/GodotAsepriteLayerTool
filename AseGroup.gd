fb043afaa5cb1f8b3f98d69cf85469dcc880446f
coinight <32589807+coinight@users.noreply.github.com>
2024-04-10 15:52:47 +0800

    Update AseGroup.gd

--- AseGroup.gd
+++ AseGroup.gd
@@ -22,12 +22,13 @@ func loadResource():
 	var error = json.parse(content)
 	if error == OK:
 		var data = json.data
-		var layer = 0
+		#var layer = 0
+		var i = 0
 		for spriteData in data["frames"]:
-			#print(spriteData)
-			#print(spriteData["filename"])
-			var meta = data["meta"]["layers"][layer]
-			print(meta)
+			print(spriteData)
+			print(spriteData["filename"])
+			#var meta = data["meta"]["layers"][layer]
+			#print(meta)
 			var node = Sprite2D.new()
 			node.name = spriteData["filename"].split("(")[1].split(")")[0]
 			node.texture = source
@@ -36,9 +37,24 @@ func loadResource():
 				spriteData["frame"]["w"],spriteData["frame"]["h"])
 			node.position = Vector2(spriteData["spriteSourceSize"]["x"] + spriteData["frame"]["w"]/2,
 				spriteData["spriteSourceSize"]["y"] + spriteData["frame"]["h"]/2)
-			node.self_modulate.a = meta["opacity"]/255
+			#node.self_modulate.a = meta["opacity"]/255
 			add_child(node)
 			node.owner = get_tree().edited_scene_root
-			node.z_index = layer
-			layer += 1
+			node.z_index = i
+			i += 1
+			#layer += 1
+		for l in data["meta"]["layers"]:
+			var n:Sprite2D = get_node(l["name"])
+			if n:
+				if l.has("opacity"):
+					n.self_modulate.a = l["opacity"]/255
+				if l.has("group"):
+					n.get_parent().remove_child(n)
+					get_node(l["group"]).add_child(n)
+					n.owner = get_tree().edited_scene_root
+			else:
+				var t = Node2D.new()
+				t.name = l["name"]
+				add_child(t)
+				t.owner = get_tree().edited_scene_root
 	set_script(null)
